3260f22d01a28905e9bef6fc7550e1002671f9cf
Ekaterina Polyntseva <ekaterina.polynceva@gmail.com>
2023-05-30 10:10:07 +0300

    added routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,26 @@
-import './App.css';
+import React from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Main from "../pages/Main/Main";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
+import Profile from "../pages/Profile/Profile";
+import Movies from "../pages/Movies/Movies";
+import SavedMovies from "../pages/SavedMovies/SavedMovies";
+
 
 function App() {
   return (
-    <div>
-      <header>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Main/>}/>
+        <Route path="/signin" element={<Login/>}/>
+        <Route path="/signup" element={<Register/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/movies" element={<Movies/>}/>
+        <Route path="/saved-movies" element={<SavedMovies/>}/>
+        <Route path="*" element={<div>404 Page Not found</div>} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/pages/SavedMovies/SavedMovies.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function SavedMovies() {
+  return (
+    <div>SavedMovies</div>
+  )
+}
+
+export default SavedMovies;
