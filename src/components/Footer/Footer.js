6c21b7a6ad326e2193380c0eb8383e643634671d
Ekaterina Polyntseva <ekaterina.polynceva@gmail.com>
2023-05-31 09:38:25 +0300

    footer adaptiv

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,15 +3,15 @@ import React from "react";
 function Footer() {
   return (
     <footer className="page footer">
-      <div className="footer__line">
-        <p className="footer__caption">Учебный проект Яндекс.Практикум х BeatFilm.</p>
-      </div>
-      <div className="footer__container">
-        <p className="footer__text">&copy; {new Date().getFullYear()}</p>
-        <div className="footer__container2">
+      <p className="footer__caption">
+        Учебный проект Яндекс.Практикум х BeatFilm.
+      </p>
+      <div className="footer__container footer__container_between footer__container_column-reverse">
+        <p className="footer__text footer__text_mobile">&copy;&nbsp;{new Date().getFullYear()}</p>
+        <span className="footer__container footer__container_end">
           <p className="footer__text">Яндекс.Практикум</p>
           <p className="footer__text">Github</p>
-        </div>
+        </span>
       </div>
     </footer>
   );
