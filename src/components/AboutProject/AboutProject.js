3209f3a3b98da6678c18346035034f0f3f06340e
Ekaterina Polyntseva <ekaterina.polynceva@gmail.com>
2023-06-01 09:29:46 +0300

    finish main desctop

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -5,7 +5,30 @@ function AboutProject(props) {
   return (
     <>
       <Title title="О проекте" />
-      <section className="about-project page__container-small"></section>
+      <section className="about-project page__container-small">
+        <article className="about-project__tabl">
+          <h3 className="about-project__tabl-title">
+            Дипломный проект включал 5 этапов
+          </h3>
+          <h3 className="about-project__tabl-title">
+            На выполнение диплома ушло 5 недель
+          </h3>
+          <p className="about-project__tabl-text">
+            Составление плана, работу над бэкендом, вёрстку, добавление
+            функциональности и&nbsp;финальные доработки.
+          </p>
+          <p className="about-project__tabl-text">
+            У&nbsp;каждого этапа был мягкий и&nbsp;жёсткий дедлайн, которые
+            нужно было соблюдать, чтобы успешно защититься.
+          </p>
+        </article>
+        <article className="about-project__date">
+          <p className="about-project__date-text about-project__date-text_bold about-project__date-text_back">1 неделя</p>
+          <p className="about-project__date-text about-project__date-text_bold about-project__date-text_front">4 недели</p>
+          <p className="about-project__date-text about-project__date-text_light">Back-end</p>
+          <p className="about-project__date-text about-project__date-text_light">Front-end</p>
+        </article>
+      </section>
     </>
   );
 }
