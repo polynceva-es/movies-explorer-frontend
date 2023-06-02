45ac801a2cea3a0fa5ea06cfabbffdbe96df08bb
Ekaterina Polyntseva <ekaterina.polynceva@gmail.com>
2023-06-02 16:12:12 +0300

    added profile

--- src/components/pages/Profile/Profile.js
+++ src/components/pages/Profile/Profile.js
@@ -7,10 +7,44 @@ function Profile(props) {
     <>
       <Header loggedIn={loggedIn} />
       <main className="profile">
-        <h1>Привет, Виталий!</h1>
-
-        <button className="profile__btn">Редактировать</button>
-        <button className="profile__btn profile__btn_logout">Выйти из аккаунта</button>
+        <h1 className="profile__title">Привет, Виталий!</h1>
+        <form className="profile__form">
+          <label className="profile__label">
+            <div className="profile__label-container">
+              <label for="#name" className="profile__input-label">
+                Имя
+              </label>
+              <input
+                className="profile__input"
+                id="name"
+                name="name"
+                type="text"
+                value="Виталий"
+              />
+            </div>
+            <span className="profile__input-error"></span>
+          </label>
+          <label className="profile__label">
+            <div className="profile__label-container">
+              <label for="#email" className="profile__input-label">
+                E-mail
+              </label>
+              <input
+                className="profile__input"
+                id="email"
+                name="email"
+                type="text"
+                value="pochta@yandex.ru"
+              />
+            </div>
+            <span className="profile__input-error"></span>
+          </label>
+          <span className="profile__input-error"></span>
+          <button className="profile__btn">Редактировать</button>
+        </form>
+        <button className="profile__btn profile__btn_logout">
+          Выйти из аккаунта
+        </button>
       </main>
     </>
   );
