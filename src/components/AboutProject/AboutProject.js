d4270571e75366edeaba789da9a9d7b0757365f4
Ekaterina Polyntseva <ekaterina.polynceva@gmail.com>
2023-06-04 19:32:08 +0300

    added adaptiv main page

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -10,13 +10,13 @@ function AboutProject(props) {
           <h3 className="about-project__tabl-title">
             Дипломный проект включал 5 этапов
           </h3>
-          <h3 className="about-project__tabl-title">
-            На выполнение диплома ушло 5 недель
-          </h3>
           <p className="about-project__tabl-text">
             Составление плана, работу над бэкендом, вёрстку, добавление
             функциональности и&nbsp;финальные доработки.
           </p>
+          <h3 className="about-project__tabl-title">
+            На выполнение диплома ушло 5 недель
+          </h3>
           <p className="about-project__tabl-text">
             У&nbsp;каждого этапа был мягкий и&nbsp;жёсткий дедлайн, которые
             нужно было соблюдать, чтобы успешно защититься.
