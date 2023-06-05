2dac43f0188e9c287544768741ec715706736a10
Ekaterina Polyntseva <ekaterina.polynceva@gmail.com>
2023-06-05 12:07:45 +0300

    added adaptiv reg log pages

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -4,14 +4,14 @@ import HeaderLogo from "../../components/HeaderLogo/HeaderLogo";
 function Form(props) {
   const buttonSubmitClassName = `form__btn-submit form__btn-submit_${props.param}`;
   return (
-    <>
+    <div className="form">
       <HeaderLogo />
       <h2 className="form__title">{props.title}</h2>
       <form className="form">
         {props.children}
         <button className={buttonSubmitClassName}>{props.labelSubmit}</button>
       </form>
-    </>
+    </div>
   );
 }
 
