d3974340fedf86a7e307768cb5271b29b78e4e56
Ekaterina Polyntseva <ekaterina.polynceva@gmail.com>
2023-09-25 09:17:49 +0300

    Добавить публичный урл

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
 import { BrowserRouter } from 'react-router-dom';
+const {PUBLIC_URL} = process.env;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
